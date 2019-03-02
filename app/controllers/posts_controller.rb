cb2301749bf8bd4ff2335a91647bd5f04c760524
rgalazb <galaz.becerra@gmail.com>
2019-03-02 11:54:42 -0300

    metodo update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,25 @@
 class PostsController < ApplicationController
   def index 
     @post = Post.new
-    @posts = Post.all
+    @posts = Post.order(:id)
   end
 
   def create 
     @post = Post.new(
       title: params[:post][:title]
     )
+    @post.save 
+    respond_to :js
+  end
+
+  def edit 
+    @post = Post.find(params[:id])
+    respond_to :js
+  end
+
+  def update 
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
     @post.save 
     respond_to :js
   end
